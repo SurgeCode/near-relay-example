938f2b7084934041cde80b4d3ecc7172ec92f4bd
Till <till.friesewinkel@googlemail.com>
2024-10-28 12:34:52 +0000

    Make example compatible with cloud relayer

--- client.ts
+++ client.ts
@@ -1,20 +1,21 @@
-import { actionCreators, encodeSignedDelegate } from "@near-js/transactions";
-import { getAccount } from "./util";
+import { actionCreators, encodeSignedDelegate } from '@near-js/transactions';
+import { getAccount } from './util';
 
-const CLIENT_ID = 'the-user.testnet'
-const CLIENT_PRIVATE_KEY = 'ed25519:4PcuqzFyK8yas151xKBoBbUe5DLCd4RcpekW5rjKTsUtabkvGqwacgmnBECDdzEQAvozG5spguF2yp5knjNjDHAt'
-const NETWORK_ID = 'testnet'
-const CONTRACT_ID = 'hello.near-examples.testnet'
-const SERVER_URL = 'http://localhost:5000/'
+const CLIENT_ID = 'the-user.testnet';
+const CLIENT_PRIVATE_KEY =
+  'ed25519:4PcuqzFyK8yas151xKBoBbUe5DLCd4RcpekW5rjKTsUtabkvGqwacgmnBECDdzEQAvozG5spguF2yp5knjNjDHAt';
+const NETWORK_ID = 'testnet';
+const CONTRACT_ID = 'hello.near-examples.testnet';
+const SERVER_URL = 'http://localhost:5000/';
 
 async function sendRelay() {
   const action = actionCreators.functionCall(
     'set_greeting',
     {
-        greeting: "hello"
-   },
-   "3000000000000"
-   );
+      greeting: 'hello',
+    },
+    '3000000000000',
+  );
 
   const account = await getAccount(NETWORK_ID, CLIENT_ID, CLIENT_PRIVATE_KEY);
 
@@ -22,14 +23,13 @@ async function sendRelay() {
     actions: [action],
     blockHeightTtl: 120,
     receiverId: CONTRACT_ID,
-})
+  });
   const res = await fetch(SERVER_URL, {
-    method: "POST",
-    mode: "cors",
-    body: JSON.stringify(Array.from(encodeSignedDelegate(signedDelegate))),
-    headers: new Headers({ "Content-Type": "application/json" }),
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify([Array.from(encodeSignedDelegate(signedDelegate))]),
+    headers: new Headers({ 'Content-Type': 'application/json' }),
   });
 }
 
 sendRelay();
-
--- server.ts
+++ server.ts
@@ -1,32 +1,44 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import { deserialize } from 'borsh';
-import { Account } from "near-api-js";
-import { SCHEMA, SignedDelegate, actionCreators } from "@near-js/transactions";
+import { Account } from 'near-api-js';
+import { SCHEMA, SignedDelegate, actionCreators } from '@near-js/transactions';
 import { getAccount } from './util';
 
 const app: Express = express();
 app.use(cors());
 app.use(express.json());
 
-const RELAYER_ID = 'the-relayer.testnet'
-const RELAYER_PRIVATE_KEY = 'ed25519:2fpNWkegHqdeJNYCzaT1rhjJSsXASjeTvfzpz5xWRQavvap8CkU2ri7mjFMrFmsL3EvTtgWfy8RNY6y66aPy7Pt6'
-const NETWORK_ID = 'testnet'
+const RELAYER_ID = 'the-relayer.testnet';
+const RELAYER_PRIVATE_KEY =
+  'ed25519:2fpNWkegHqdeJNYCzaT1rhjJSsXASjeTvfzpz5xWRQavvap8CkU2ri7mjFMrFmsL3EvTtgWfy8RNY6y66aPy7Pt6';
+const NETWORK_ID = 'testnet';
 
 app.post('/', async (req: Request, res: Response) => {
-        const serializedTx: Buffer = req.body;
-        const deserializedTx: SignedDelegate = deserialize(SCHEMA.SignedDelegate, Buffer.from(serializedTx)) as SignedDelegate;
-        const relayerAccount: Account = await getAccount(NETWORK_ID, RELAYER_ID, RELAYER_PRIVATE_KEY);
-        
-        const receipt = await relayerAccount.signAndSendTransaction({
-            actions: [actionCreators.signedDelegate(deserializedTx)],
-            receiverId: deserializedTx.delegateAction.senderId
-        });
-        
-        res.json({ message: 'Relayed', data: receipt });   
+  const serializedTxs: Buffer[] = req.body;
+  const relayerAccount: Account = await getAccount(
+    NETWORK_ID,
+    RELAYER_ID,
+    RELAYER_PRIVATE_KEY,
+  );
+
+  const receipts = await Promise.all(
+    serializedTxs.map(async (serializedTx) => {
+      const deserializedTx: SignedDelegate = deserialize(
+        SCHEMA.SignedDelegate,
+        Buffer.from(serializedTx),
+      ) as SignedDelegate;
+
+      return await relayerAccount.signAndSendTransaction({
+        actions: [actionCreators.signedDelegate(deserializedTx)],
+        receiverId: deserializedTx.delegateAction.senderId,
+      });
+    }),
+  );
+
+  res.json({ message: 'Relayed', data: receipts });
 });
 
-
 app.listen(5000, () => {
-    console.log(`Relayer is running on port 5000`);
+  console.log(`Relayer is running on port 5000`);
 });
